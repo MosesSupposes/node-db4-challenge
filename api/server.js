4c3dc3b45bedcc7d0c7f3ec9a46c5a7d727cff58
Moses A. Samuel <mosamuel98@gmail.com>
2019-10-10 16:39:52 -0700

    use ingredients router on server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const recipesRouter = require('./routes/recipesRouter')
+const ingredientsRouter = require('./routes/ingredientsRouter')
 
 const server = express()
 
 server.use(express.json())
 server.use('/api/recipes', recipesRouter)
+server.use('/api/ingredients', ingredientsRouter)
 
 module.exports = server

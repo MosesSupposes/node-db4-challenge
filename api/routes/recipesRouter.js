d9ac9497230f1b3c8d17d11dbe054f170a0cedd5
Moses A. Samuel <mosamuel98@gmail.com>
2019-10-10 16:53:36 -0700

    send a response status on GET handler

--- api/routes/recipesRouter.js
+++ api/routes/recipesRouter.js
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) => {
     .then(recipes => {
         (!recipes.length)
             ? res.status(404).json({error: "There are no recipes in the database yet"})
-            : res.json(200).json(recipes)
+            : res.status(200).json(recipes)
     })
     .catch(err => {
         res.status(404).json({error: "There are no recipes in the database yet"})

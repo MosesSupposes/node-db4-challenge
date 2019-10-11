0c3bcf4b717aca70b8b58dbc449c221a4caaf370
Moses A. Samuel <mosamuel98@gmail.com>
2019-10-10 19:17:40 -0700

    add  a route for getting the instructions for a recipe

--- api/routes/recipesRouter.js
+++ api/routes/recipesRouter.js
@@ -30,6 +30,29 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.get('/:id/instructions', (req, res) => {
+    RecipesModel
+    .getInstructions(req.params.id)
+    .then(recipes => {
+        res.status(200).json(recipes)
+    })
+    .catch(err => {
+        res.status(404).json({error: "Couldn't find instructions for the recipe with the specified Id."})
+    })
+})
+
+router.get('/:id/shopping_list', (req, res) => {
+    RecipesModel
+    .getShoppingList(req.params.id)
+    .then(recipes => {
+        res.status(200).json(recipes)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(404).json({error: "Couldn't find the recipe with the specified Id."})
+    })
+})
+
 router.post('/', async (req, res) => {
     const [err, newRecipe] = await withCatch( RecipesModel.insert(req.body) )
 

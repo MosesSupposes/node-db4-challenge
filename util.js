3b78373f73ac636392d32a9cc236d4ea76984235
Moses A. Samuel <mosamuel98@gmail.com>
2019-10-10 18:12:51 -0700

    add a utility for filtering objects

--- util.js
+++ util.js
@@ -1,9 +1,20 @@
 module.exports = {
-    withCatch
+    withCatch,
+    filterObj
 }
 
 function withCatch(promise) {
     return promise
         .then(data => [null, data])
         .catch(err => [err])
+}
+
+function filterObj (predicateFn, o) {
+    const newObj = {}
+    
+	Object.keys(o)
+	.filter(key => predicateFn(key))
+    .forEach(key => newObj[key] = o[key])
+    
+	return newObj
 }
